2db298de04ecee8f81b3cf142fea7e28113ee835
Alexis-zn <alexis.zentici@gmail.com>
2021-02-16 15:11:32 +0100

    exo done

---
+++ decimal.js
@@ -0,0 +1,5 @@
+let allNumbers = [0x123, 0123, 0b10011001, 0xdeadbeef, 0xea7beef, 0b1111111111111111, 0777]
+    for(i= 0; i < allNumbers.length ; i++)
+      console.log(allNumbers[i].toString(10))
+
+        
