06b9a1a888240ad65aa72faa79c433167b6789d6
Alexis-zn <alexis.zentici@gmail.com>
2021-02-16 17:10:41 +0100

    pas opti

--- convert.js
+++ convert.js
@@ -2,4 +2,5 @@ let allNumbers = [10, 15, 16, 5005, 52390903]
   for(i= 0; i < allNumbers.length; i++) {
     console.log(allNumbers[i].toString(2)) 
     console.log(allNumbers[i].toString(8)) 
-    console.log(allNumbers[i].toString(16)) }
+    console.log(allNumbers[i].toString(16)) 
+  }
