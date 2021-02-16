0f73e1186c51c1aaa1fb68e0a701838bc145a8ca
Alexis-zn <alexis.zentici@gmail.com>
2021-02-16 17:05:52 +0100

    exo done

--- showChar.js
+++ showChar.js
@@ -1,5 +1,12 @@
+let autor= 'autor'
+let book= 'book'
 let msg = "C'était à Mégara, faubourg de Carthage, dans les jardins d'Hamilcar."
+
+console.log(`This text is an extract for the book ${book} , written by ${autor}`)
+console.log(`This text has ${msg.length} characters`)
+
 for(i=0; i < msg.length; i++) 
   if(msg[i] !== " ") {
 console.log(`${msg[i]} is at index ${i}`) 
 }
+
